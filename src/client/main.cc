93109870498e6043211f8ccbe90efaf790557883
Kuntal Majumder <hellozee@disroot.org>
2020-02-23 19:39:50 +0530

    client: can pass different server ips

--- src/client/main.cc
+++ src/client/main.cc
@@ -3,7 +3,19 @@
 auto
 main(int argc, char const* argv[]) -> int
 {
-    lzr::repl laser_repl("127.0.0.1");
-    laser_repl.run();
+    if (argc != 2) {
+        std::cout << "Usage :" << std::endl
+                  << "\tlzr-client <server-ip>" << std::endl
+                  << "\tExample: lzr-client 127.0.0.1" << std::endl;
+        return -1;
+    }
+
+    try {
+        lzr::repl laser_repl(argv[1]);
+        laser_repl.run();
+    } catch (std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
+
     return 0;
 }
